f508fb731de84087557be15c3ce14f88141fde28
Jonas Caceres Marte <Jonad.caceres@gmail.com>
2020-10-26 14:22:24 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import 'antd/dist/antd.css';
 import './App.css';
+
+import { Layout  } from 'antd';
+const {  Content,  } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content style={{ padding: '0 50px' }}>
+    <div className="site-layout-content"></div>
+  </Content>
+  
   );
 }
 
